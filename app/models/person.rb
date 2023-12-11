93f38771294dcd9209b87a0477609a828e420664
leePettigrew <150901334+leePettigrew@users.noreply.github.com>
2023-12-11 02:13:17 +0000

    finished user crud
    Finished edit,view, and add modals alongwith error checking for input fields.
    
    user view modal needs visual upgrades.
    
    Checkpoint for devise.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,22 @@
 class Person < ApplicationRecord
+  # Validating presence of attributes
+  validates :first_name, :last_name, :gender, :age, :user_name, :email, 
+            :phone, :address_street, :address_county, :college, :course, :year, presence: true
+
+  # Validating that first name and last name only contain letters
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+
+  # Validating email format and uniqueness
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  # Validating that age is a number greater than 0
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+
+  # Validating phone number format (assuming a 10-digit format)
+  validates :phone, format: { with: /\A\d{10}\z/, message: "must be 10 digits" }
+
+  # Validating that year is a number and not greater than the current year
+  validates :year, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year }
+
+  # Add other specific validations as needed
 end
