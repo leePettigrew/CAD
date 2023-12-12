d810365f3266aa14fa0c20f17457ab9d193b61f1
leePettigrew <150901334+leePettigrew@users.noreply.github.com>
2023-12-12 00:20:58 +0000

    Checkpoint for cloud deployment
    Added github api call to the about me page.
    
    views recent changes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,34 @@
 class HomeController < ApplicationController
+  require 'net/http'
+  require 'json'
+
   def index
+    # ...
   end
 
   def about
+    @about_me = "My Name is Lee Pettigrew"
+    repo_uri = URI("https://api.github.com/repos/leePettigrew/CAD")
+    commits_uri = URI("https://api.github.com/repos/leePettigrew/CAD/commits")
+
+    # Fetch repository data
+    repo_response = Net::HTTP.get_response(repo_uri)
+    @repo_data = JSON.parse(repo_response.body) if repo_response.is_a?(Net::HTTPSuccess)
+
+    # Fetch commits data
+    commits_response = Net::HTTP.get_response(commits_uri)
+    if commits_response.is_a?(Net::HTTPSuccess)
+      @commits_data = JSON.parse(commits_response.body)
+    else
+      @commits_data = []
+      logger.error "GitHub Commits API response not successful: #{commits_response.message}"
+    end
+  rescue StandardError => e
+    logger.error "Failed to fetch GitHub data: #{e.message}"
   end
 
-  
+
+
+
+
 end
